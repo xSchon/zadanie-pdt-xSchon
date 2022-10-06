62bd3872db80d6a2d4aff46c15abdb11fa31ea16
Martin Schön <martin.schon@softpoint.sk>
2022-10-07 01:07:26 +0200

    Add documentation

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from utils import config, utilities
 config.initialize_logger()
 import gc
-import gzip
-import os
-import json
 import logging
-
-import pandas as pd
 
 from fill_tables.fill_authors import Fill_authors
 from fill_tables.fill_database import Fill_database
